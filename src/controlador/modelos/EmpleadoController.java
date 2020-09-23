9e89abc3e60c8a29cfa47380b4a958271c3261ff
Raul Lora Gomez <lgraul87@gmail.com>
2020-09-23 02:59:17 +0200

    BBDD comprobada

--- src/controlador/modelos/EmpleadoController.java
+++ src/controlador/modelos/EmpleadoController.java
@@ -1,11 +1,27 @@
 package controlador.modelos;
 
+import controlador.database.ConexionDB;
 import laboral.Empleado;
 
 public class EmpleadoController {
 
 	public void addEmployeeE(Empleado oEmpleado1) {
-		// TODO Auto-generated method stub
+
+		String sNombre = oEmpleado1.getsNombre();
+		String sDni = oEmpleado1.getsDni();
+		char cLetra = oEmpleado1.getcLetra();
+		byte bCategoria = oEmpleado1.getbCategoria();
+		byte bAnio = oEmpleado1.getbAnyosTrabajados();
+
+		String sql = "SELECT DNI FROM EMPLEADO WHERE DNI = '" + sDni + "'";
+
+		if (ConexionDB.executeCount(sql) == 0) {
+
+			String sql2 = "INSERT INTO EMPLEADO VALUES ('" + sNombre + "'," + bCategoria + "," + bAnio + ",'" + sDni
+					+ "','" + cLetra + "')";
+
+			ConexionDB.executeUpdate(sql2);
+		}
 
 	}
 
@@ -31,7 +47,7 @@ public class EmpleadoController {
 
 	public void upDateSalaryAll() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public Empleado bringEmployee(Empleado oEmpleado) {
@@ -41,7 +57,7 @@ public class EmpleadoController {
 
 	public void upDateSalary(Empleado oEmpleado, Empleado oEmpleadoEditado) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
