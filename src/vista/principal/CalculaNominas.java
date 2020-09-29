82b96339402c4869989d6d7e563bbd7dd042b203
Raul Lora Gomez <lgraul87@gmail.com>
2020-09-29 14:30:27 +0200

    fignore

--- src/vista/principal/CalculaNominas.java
+++ src/vista/principal/CalculaNominas.java
@@ -84,7 +84,7 @@ public class CalculaNominas {
 		 * convenientemente el código generando el Javadoc.
 		 */
 
-		// Gitignore ?72¿
+		// Gitignore ?727¿
 		byte bAnios;
 		byte bCategoria;
 		char cLetra = 0;
